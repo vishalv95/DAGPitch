1484b7cb28d249b2a4308c15bd74c301d43cf02a
vishalv95 <vishalv95@gmail.com>
2015-04-03 21:13:24 -0500

    Removed unnecessary files and optimized regressions
    removed extraneous files in the directory
    calculated regressions on the regression matrix directly because rbind
    is too slow

--- regressions/regressionsGen.R
+++ regressions/regressionsGen.R
@@ -1,12 +1,19 @@
 library(tseries)
-library(MASS)
+library(zoo)
+
 #Load Security data
 companies <- read.table('sp500-symbol-list.txt')
+
 colnames(companies) <- c('symbol')
 prices <- matrix(0, 252, 0)
 for (comp in companies$symbol){
-  price <- data.frame(get.hist.quote(comp, quote='Adj', start='2014-04-03'))
+  price <- try(myghq(comp, quote='Adj', start='2014-04-03'))
+  if(class(price)=="try-error") 
+    next
+  price <- data.frame(price)
   colnames(price) <- comp
+  
+  #What to do if the stock price has no data here?
   max.len <- 252
   rep <- max.len - length(price[,comp])
   price <- c(price[,comp], rep(NA, if(rep > 0) rep else 0))
@@ -14,16 +21,21 @@ for (comp in companies$symbol){
   colnames(prices)[length(colnames(prices))] <- comp
 }
 
-regressions <- data.frame(matrix(0, 0, 3))
-for(compA in colnames(prices)){
-  for(compB in colnames(prices)){
-    if(compA != compB){
-      row <- matrix(0, 1, 3)
-      row[1,1] <- compA
-      row[1,2] <- compB
-      row[1,3] <- cor(prices[,compA], prices[,compB])
-      regressions <- rbind(regressions,row)
-    }
-  }
+combs <- combn(colnames(prices),2)
+# write.csv(data.frame(combs),'pairs.csv')
+regressions <- data.frame(matrix(0, ncol(combs), 4))
+# beg <- Sys.Time()
+for(c in 1:ncol(combs)){
+  compA <- combs[1,c]
+  compB <- combs[2,c]
+  regressions[c,1] <- compA
+  regressions[c,2] <- compB
+  regressions[c,3] <- cor(prices[,compA], prices[,compB], use="pairwise.complete.obs")
+  regressions[c,4] <- min(length(na.omit(prices[,compA])), length(na.omit(prices[,compA])))
+  if(c%%100==0) print(c) 
 }
-colnames(regressions) <- c('comp_A', 'comp_B', 'r-value')
+# end <- Sys.Time()
+# print(end - beg)
+
+colnames(regressions) <- c('comp_A', 'comp_B', 'r_value', 'n')
+write.csv(regressions, 'regressions.csv', row.names=F)
